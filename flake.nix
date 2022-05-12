31572d5cb675afb1c8c7ce150c693799ac35866b
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:57 -0300

    chore: re index decimal128

--- flake.nix
+++ flake.nix
@@ -7,6 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."decimal128-master".dir   = "master";
+  inputs."decimal128-master".owner = "nim-nix-pkgs";
+  inputs."decimal128-master".ref   = "master";
+  inputs."decimal128-master".repo  = "decimal128";
+  inputs."decimal128-master".type  = "github";
+  inputs."decimal128-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."decimal128-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."decimal128-v0_1_0".dir   = "v0_1_0";
+  inputs."decimal128-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."decimal128-v0_1_0".ref   = "master";
+  inputs."decimal128-v0_1_0".repo  = "decimal128";
+  inputs."decimal128-v0_1_0".type  = "github";
+  inputs."decimal128-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."decimal128-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."decimal128-v0_1_1".dir   = "v0_1_1";
+  inputs."decimal128-v0_1_1".owner = "nim-nix-pkgs";
+  inputs."decimal128-v0_1_1".ref   = "master";
+  inputs."decimal128-v0_1_1".repo  = "decimal128";
+  inputs."decimal128-v0_1_1".type  = "github";
+  inputs."decimal128-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."decimal128-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."decimal128-v0_1_2".dir   = "v0_1_2";
+  inputs."decimal128-v0_1_2".owner = "nim-nix-pkgs";
+  inputs."decimal128-v0_1_2".ref   = "master";
+  inputs."decimal128-v0_1_2".repo  = "decimal128";
+  inputs."decimal128-v0_1_2".type  = "github";
+  inputs."decimal128-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."decimal128-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
